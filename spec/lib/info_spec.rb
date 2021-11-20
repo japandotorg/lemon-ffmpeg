43ad724a6ea238670c07ee7cdf36185935224f86
Lemon Rose <78662983+japandotorg@users.noreply.github.com>
2021-11-21 01:29:49 +0530

    test & spec files

---
+++ spec/lib/info_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'ffmpeg_video_info'
+require 'ostruct'
+describe FFmpeg::Video do
+    subject { FFmpeg::Video }
+    context 'when .info' do
+        context 'with wrong args' do
+            specify do
+                expect { FFmpeg::Video.info('') }.to raise_error('ffmpeg: Unable to open input file')
+            end
+        end
+        context 'with proper args' do
+            subject { OpenStruct.new(FFmpeg::Video.info('')) }
+            it 'prints proper data' do
+                expect(subject.format_name).to eql 'mov,mp4,m4a,3gp,3g2,mj2'
+                expect(subject.file_name).to eql ''
+            end
+        end
+    end
+end
